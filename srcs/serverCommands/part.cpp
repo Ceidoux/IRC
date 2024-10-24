fba6a1d56217d16d6840ed6f620dd5b2de5ca226
Jeremie Leguay <jleguay@student.42.fr>
2024-10-24 10:45:00 +0200

    fix: part

--- srcs/serverCommands/part.cpp
+++ srcs/serverCommands/part.cpp
@@ -2,26 +2,31 @@
 
 void	Server::part( std::string line, Client & client )
 {
-    std::string channel = line.substr(line.find("#"), line.find(" ", line.find("#")) - line.find("#"));
-    std::string partMessage = line.substr(line.find(":", line.find("#")) + 1);
-    std::string RPL_PART = ":" + client.getNick() + "!" + client.getUser() + "@localhost PART " + channel + "\r\n";
-	for (std::size_t i = 0; i < this->_channels.size(); i++)
+    if (line.find("#") == std::string::npos)
+		writeRPL(client.getFd(), ERR_NOSUCHCHANNEL(client.getNick(), line.substr(line.find(" "))));
+	else
 	{
-		if (this->_channels[i].getName() == channel)
+		std::string channel = line.substr(line.find("#"), line.find(" ", line.find("#")) - line.find("#"));
+		std::string partMessage = line.substr(line.find(":", line.find("#")) + 1);
+		std::string RPL_PART = ":" + client.getNick() + "!" + client.getUser() + "@localhost PART " + channel + "\r\n";
+		for (std::size_t i = 0; i < this->_channels.size(); i++)
 		{
-			for (std::size_t j = 0; j < this->_channels[i].getClients().size(); j++)
+			if (this->_channels[i].getName() == channel)
 			{
-				if (this->_channels[i].getClients()[j]->getNick() == client.getNick())
+				for (std::size_t j = 0; j < this->_channels[i].getClients().size(); j++)
 				{
-					for (std::size_t k = 0; k < this->_channels[i].getClients().size(); k++)
-						writeRPL(this->_channels[i].getClients()[k]->getFd(), RPL_PART);
-					this->_channels[i].removeClient(&client);
-					if (this->_channels[i].getNumberOfClients() == 0)
+					if (this->_channels[i].getClients()[j]->getNick() == client.getNick())
 					{
-						this->_channels.erase(this->_channels.begin() + i);
-						std::cout << this->_channels[i].getName() << " was removed (no client left)" << std::endl;
-					}					
-					break;
+						for (std::size_t k = 0; k < this->_channels[i].getClients().size(); k++)
+							writeRPL(this->_channels[i].getClients()[k]->getFd(), RPL_PART);
+						this->_channels[i].removeClient(&client);
+						if (this->_channels[i].getNumberOfClients() == 0)
+						{
+							this->_channels.erase(this->_channels.begin() + i);
+							std::cout << this->_channels[i].getName() << " was removed (no client left)" << std::endl;
+						}					
+						break;
+					}
 				}
 			}
 		}
